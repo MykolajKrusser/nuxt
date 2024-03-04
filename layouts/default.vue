a8e120bf1dfe25a5c4664eefc326e199f08b65a1
mkrusser <mkrusser@goldencomm.com>
2024-03-04 15:33:27 +0100

    init

---
+++ layouts/default.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="container mx-auto">
+    <header>
+      Hello
+    </header>
+    <main>
+      <Nuxt />
+    </main>
+    <footer>
+      footer
+    </footer>
+  </div>
+</template>
+
+<script setup lang="ts">
+</script>
---
+++ pages/beer.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    Beer list
+  </div>
+</template>
+
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'Beer',
+})
+</script>
